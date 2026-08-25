d9e67ca3df5318a2fe46cad9afdc225ab862ff5f
akash-senthil-06 <akashsenthil029@gmail.com>
2026-08-25 09:51:09 +0530

    2nd problem solved

--- src/LinkedList/linkedlist.java
+++ src/LinkedList/linkedlist.java
@@ -11,5 +11,21 @@ public class linkedlist{
         subjects.add("Maths");
         subjects.add("Science");
         System.out.println("First subject -> "+subjects.getFirst()+"\n"+"Last subject -> "+subjects.getLast());
+
+        //2nd
+        LinkedList<Integer> nums=new LinkedList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+        nums.add(5);
+        System.out.println("Before adding: "+nums);
+        nums.push(0);
+        nums.push(11);
+        nums.push(22);
+        nums.addLast(6);
+        nums.addLast(7);
+        nums.addLast(8);
+        System.out.println("After adding: "+nums);
     }
 }
